d48d577e6d84dffce86d93bc6b1f191011c09942
neophtye1001 <pitlavinay0077@gmail.com>
2020-10-31 12:56:08 +0530

    Analog Clock

---
+++ site/script.js
@@ -0,0 +1,31 @@
+// xicos49916@jarilusua.com     Audios on mail 
+
+//Accessing the elements 
+const secHand = document.querySelector('.seconds');
+const minHand = document.querySelector('.minute');
+const hourHand = document.querySelector('.hour');
+
+function setDate(){
+    const now = new Date(); //Creating the object of Date.
+    const sec = now.getSeconds(); //Getting seconds using date obj.
+    const secDegree = ( (sec / 60) * 360 ) + 90; //Rotates 1deg each minute.
+    // console.log(secDegree);
+    secHand.style.transform = `rotate(${secDegree}deg)`;    
+    console.log(sec);
+
+    const min = now.getMinutes();
+    console.log(min);
+    const minDegree = ( (min / 60) * 360 ) + 90;
+    minHand.style.transform = `rotate(${minDegree}deg)`;
+
+    const hour= now.getHours();
+    console.log(hour);
+    const hourDegree = ( ( (hour) / 12) * 360 ) + 90;
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
+
+    if(secDegree == 0 ){
+        secHand.style.transform = 'none';
+    }
+    
+}
+setInterval(setDate,1000);
